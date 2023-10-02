241a06692934e6cac71b74bb3c533718b1672ee7
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2023-10-02 16:21:22 -0300

    add hotwire

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,12 @@
 //= require bootstrap
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../controllers";
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+// Turbo.session.drive = true
